514c48c99337a2f3258952de2c6d9c22f8677afc
Denzel <114736457+denzelesh@users.noreply.github.com>
2023-03-05 17:00:19 +0000

    Create the program.cs file & added my name as auth

---
+++ program.cs
@@ -0,0 +1,17 @@
+class DistanceConverter
+{
+
+    static void AuthorName()
+    {
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("   App 01 - Distance Converter");
+        Console.WriteLine("         By Denzel Eshun");
+        Console.WriteLine("-----------------------------------");
+    }
+
+
+    static void Main() 
+    {
+        AuthorName();
+    }
+}
